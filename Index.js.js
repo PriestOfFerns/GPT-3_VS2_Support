65eb2af34bf93369581f3cddd15cff8cd8d36b3a
PriestOfFerns <timofeydenin@gmail.com>
2023-01-31 00:29:32 +0300

    Actually coded it

--- Index.js.js
+++ Index.js.js
@@ -1,62 +1,44 @@
-import { Configuration, OpenAIApi } from "openai";
+require('dotenv').config();
+
+const { Configuration, OpenAIApi } = require("openai");
+
+
+
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-export default async function (req, res) {
-  if (!configuration.apiKey) {
-    res.status(500).json({
-      error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
-    });
-    return;
-  }
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
-    });
-    return;
-  }
 
-  try {
+const prefix = `Answer the question as truthfully as possible using the provided text, and if the answer is not contained within the text below answer that you don't know. Make sure to vary your answers so it doesn't sound robotic."
+
+Context:
+Optifine shouldn't be used. Optifine is an awful optimization mod. It messes with rendering and it's closed source nature prevents people from making compat for it. Rubidium should be used instead of Optifine
+
+Q:`
+
+client.on('messageCreate', async msg => {
+
+  if (msg.channel.id ==  "1069715100434432100" && msg.author.bot == false) {
+    console.log(msg.content)
     const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+      model: "text-curie-001",
+      prompt: prefix +msg.content+"\n" + "A:",
+      temperature: 0.05,
+      max_tokens: 120
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
-        }
-      });
-    }
+    console.log(completion.data.choices)
+    msg.reply(completion.data.choices[0].text)
   }
-}
+    
+});
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
-}
+client.login(process.env.DISCORD_TOKEN);
