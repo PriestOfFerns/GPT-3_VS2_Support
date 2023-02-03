0e9981e3a8b9c4d757c2354eee0b0d3a694ce942
PriestOfFerns <timofeydenin@gmail.com>
2023-02-03 18:46:17 +0300

    Embeddings are practically finished.
    Just gotta change the prompt to be less shit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('fs')
 
 const { Configuration, OpenAIApi } = require("openai");
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, SlashCommandSubcommandBuilder } = require('discord.js');
 const { readFileSync, fstat, writeFileSync } = require('fs');
 const { off } = require('process');
 
@@ -22,24 +22,50 @@ client.on('ready', () => {
 });
 
 
-const prefix = `Answer the question as truthfully as possible using the provided text, and if the answer is not contained within the text below answer that you don't know. Make sure to vary your answers so it doesn't sound robotic."
+const prefix = `Answer the question as truthfully as possible using the provided text, and if the answer is not contained within the text below answer that you don't know.
 
-Context:
-Optifine shouldn't be used. Optifine is an awful optimization mod. It messes with rendering and it's closed source nature prevents people from making compat for it. Rubidium should be used instead of Optifine
+Context: `
 
-Q:`
+
 
 client.on('messageCreate', async msg => {
 
+ 
+
   if (msg.channel.id ==  "1069715100434432100" && msg.author.bot == false) {
+
+    const ConJSON =  JSON.parse(readFileSync("Context.json"))
+    const Ordered = await order_document_sections_by_query_similarity(msg.content, Embeds) 
+
+
+    let Context = "\n"
+    for (x in [0,1,2]) {
+      const EmbOrdered = Ordered[x][1].split(",")
+ 
+   
+  
+      
+      for (x of ConJSON) {
+      
+        if (x.Title == EmbOrdered[0] && x.Header == EmbOrdered[1]) {
+          Context+="*"+x.Content+"\n\r"
+          break
+          
+        }
+      }
+    }
+    
+    
     console.log(msg.content)
+    const pref = prefix + Context + "\r\nQ: " +msg.content +"\r\nA:"
+    console.log(pref)
     const completion = await openai.createCompletion({
       model: "text-curie-001",
-      prompt: prefix +msg.content+"\n" + "A:",
+      prompt: pref,
       temperature: 0.05,
       max_tokens: 120
     });
-    console.log(completion.data.choices)
+    
     msg.reply(completion.data.choices[0].text)
   }
     
@@ -135,16 +161,16 @@ async function order_document_sections_by_query_similarity(query, contexts){
 const data = JSON.parse(readFileSync("Context.json"))
 const Embeds = load_embeddings("ContextEmbedding.json")
 
-order_document_sections_by_query_similarity("Should I use optifine?", Embeds).then(tab=>console.log(tab))
 
-/* Creates a "ContextEmbedding.json" file"
+
+ // Creates a "ContextEmbedding.json" file"
 
 
 create_embeddings(data).then(Embs=>{
   Jfile = JSON.stringify(Embs)
   writeFileSync("ContextEmbedding.json",Jfile)
 })
-*/
 
 
-//client.login(process.env.DISCORD_TOKEN);
+
+client.login(process.env.DISCORD_TOKEN);
